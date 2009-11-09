c729d2fa8ba36bffc4fd7120918b111a63785592
Jean-Christophe Baptiste <jc@phocean.net>
2009-11-09 23:56:41 +0100

    comments cleanup

--- package/sshclass.py
+++ package/sshclass.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-#===============================================================================
-#    Sshclass is a module to be used with Netios.
-#    It is the interface based on Pexpect (http://pexpect.sourceforge.net/pexpect.html)
-#    that handles the SSH connection process.
-#
-#    Netios is a tool to mass configure a park of cisco devices.
-#    Its primary feature is password updating, but it can be extended if
-#    you provide it with a file containing any cisco command you wish.
-#    Copyright (C) 2009  Jean-Christophe Baptiste
-#    (jc@phocean.net, http://www.phocean.net)
-#
-#    All the credits go to the Pexpect developpers, which is a great module.
-#    Plese check http://pexpect.sourceforge.net/pexpect.html
-# 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-# 
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-# 
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#===============================================================================
+"""
+    Sshclass is a module to be used with Netios.
+    It is the interface based on Pexpect (http://pexpect.sourceforge.net/pexpect.html)
+    that handles the SSH connection process.
+
+    Netios is a tool to mass configure a park of cisco devices.
+    Its primary feature is password updating, but it can be extended if
+    you provide it with a file containing any cisco command you wish.
+    Copyright (C) 2009  Jean-Christophe Baptiste
+    (jc@phocean.net, http://www.phocean.net)
+
+    All the credits go to the Pexpect developpers, which is a great module.
+    Plese check http://pexpect.sourceforge.net/pexpect.html
+ 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import pexpect, sys, os, re, datetime
 
-#===============================================================================
-# horodating function
-# according to the receive flag, it returns a format for a log file entry or the
-# file name itself
-#===============================================================================
 def time (flag):
+	"""
+	Timestamp function
+	The flag determines if it returns a timestamp formatted for a log file itself or its content
+	"""
 	if flag:
 		hor=datetime.datetime.now()
 		hor=hor.strftime("%b  %d %H:%M:%S ")
@@ -46,13 +45,15 @@ def time (flag):
 		hor=hor.strftime("%Y-%m-%dT%H%M")
 		return (hor)
 
-#===============================================================================
-#  Generic SSH class based on pexpect, managing the connection
-#===============================================================================
-
 class sshConn:
+	"""
+	This class handles a generic SSH connection, using the Pexpect library
+	"""
 	
 	def __init__ (self,host,user,password,log,startTime,logincount, debug):
+		"""
+		Initialize the sshConn instance and fix a generic prompt that is supposed to work with most SSH servers
+		"""
 		self.host=host
 		self.user=user
 		self.password=password
@@ -63,8 +64,11 @@ class sshConn:
 		self.logincount=logincount
 		self.debug=debug
 
-# --- print error messages
 	def error (self,type):
+		"""
+		Snippet to handle error messages (except)
+		Return 1
+		"""
 		if type == 'timeout':
 			print "### SSH Timeout : check that the SSH service is on"
 			self.log.write ("%sCould not open SSH socket\n"%time(1))
@@ -89,8 +93,11 @@ class sshConn:
 		self.ssh.kill(0)
 		return 1
 
-# --- process ssh login
 	def login (self,verb):
+		"""
+		Handle the SSH Login
+		Return 0
+		"""
 		try:
 			self.ssh = pexpect.spawn ('ssh %s@%s'%(self.user,self.host))
 			#self.ssh.logfile = sys.stdout
@@ -102,37 +109,37 @@ class sshConn:
 				self.ssh.logfile = fout
 			print "~ SSH session n°%d"%self.logincount
 			i = self.ssh.expect (["assword:", r"yes/no"],timeout=7)
-	# --- prompted for password
+			# --- prompted for password
 			if i==0:
 				if verb:
 					print ">>> Authenticating"
 				self.ssh.sendline(self.password)
 			elif i==1:
-	# --- prompted for key
+				# --- prompted for key
 				if verb:
 					print ">>> Key request"
 				self.ssh.sendline("yes")
 				self.ssh.expect("assword", timeout=7)
 				self.ssh.sendline(self.password)
-	# --- prompt after password input : denied or choice for a terminal type
+			# --- prompt after password input : denied or choice for a terminal type
 			i = self.ssh.expect (['Permission denied', 'Terminal type', self.prompt, 'assword'],timeout=15)
-	# --- permission denied : call to error function
+			# --- permission denied : call to error function
 			if i == 0:
 				return (self.error('denied'))
 			elif i == 1:
-	# --- send terminal type
+				# --- send terminal type
 				if verb:
 					print '>>> SSH Login OK... need to send terminal type.'
 				self.ssh.sendline('vt100')
 				self.ssh.expect (self.prompt)
 			elif i == 2:
-	# --- login successful
+			# --- login successful
 				if verb:
 					print '>>> SSH Login OK.'
 			elif i == 3:
-	# --- wrong username
+			# --- wrong username
 				return (self.error('user'))
-        # --- deactivate echo
+			# --- deactivate echo
 			self.ssh.setecho("False")
 			return 0
 		except pexpect.TIMEOUT:
@@ -142,10 +149,14 @@ class sshConn:
 		except KeyboardInterrupt:
 			self.error ('keyboard')
 
-# --- interactive
 	def interactive(self):
+		"""
+		Interactive SSH session
+		return 0
+		"""
 		try:
 			self.ssh.interact()
+			return 0
 		except pexpect.TIMEOUT:
 			self.error ('timeout')
 		except pexpect.EOF:
@@ -153,10 +164,15 @@ class sshConn:
 		except KeyboardInterrupt:
 			self.error ('keyboard')
 
-# --- close ssh connection
 	def ssh_close(self,ct):
+		"""
+		Close the SSH connection
+		Exit works in most cases
+		A flag can be activated if it is a Cisco router still in <configure> mode that we need to logout
+		Return 0
+		"""
 		try:
-	# --- flag indicates that end is necessary before closing (conf t)
+			# --- flag indicates that end is necessary before closing (conf t)
 			if ct==1 :
 				self.ssh.sendline ('end')
 				self.ssh.expect(self.prompt)
@@ -174,4 +190,8 @@ class sshConn:
 			self.error ('keyboard')
 
 	def __del__(self):
+		"""
+		Clear the instance
+		No special handling
+		"""
 		pass
