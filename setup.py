f9c8e77d3f25d98758e577acc7ac89bd4907d6d5
Jean-Christophe Baptiste <jc@phocean.net>
2010-02-10 13:19:05 +0100

    add install sources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = "netios",
-  version = "0.72",
+  version = "0.73",
   description = "SSH remote configuration tool",
   author = "Jean-Christophe Baptiste",
   author_email = "jc@phocean.net",
@@ -11,7 +11,7 @@ setup(
   package_dir = {'': 'lib'},
   packages = ['netios'],
   scripts = ["netios"],
-  long_description = """ Cisco configuration tool through SSH """,
+  long_description = """ Netios is a little tool operating Cisco routers within SSH. """,
   license = "GNU GPL v2",
   platforms = "noarch"
 )
